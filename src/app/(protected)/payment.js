dd9967973b9199eb7bdf549c9f54a67d9dfdc61f
brendagfs <brendagdsantos@gmail.com>
2024-10-02 15:44:18 +0000

    [feat] aula 12 (13:00 sla)

--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -1,9 +1,46 @@
-import {Text, View} from "react-native";
+import { router } from "expo-router";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function List() {
+export default function Payments() {
     return (
-    <View style={{ flex:1, justifyContent: "center", alignItems: "center"}}>
-            <Text> Listagem </Text>
-    </View>
+        <View style={styles.content}>
+            <View styles={styles.inputView}>
+                <TextInput placeholder="valor" />
+            </View>
+            <View styles={styles.inputView}>
+                <TextInput placeholder="Usuario" />
+            </View>
+            <View styles={styles.inputView}>
+                <TextInput placeholder="Data" />
+            </View>
+            <View styles={styles.inputView}>
+                <TextInput placeholder="Observaçoes" />
+            </View>
+            <View styles={styles.inputView}>
+                <Button title="Salvar" />
+                <Button title="Continuar" />
+                <Button title="Cancelar" onPress={()=> router.back()} />
+            </View>
+        </View>
     );
 }
+const styles = StyleSheet.create({
+    content: {
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        padding: 10,
+    },
+    inputView: {
+        borderColor: "black",
+        borderWidth: 1,
+        width: "100%",
+        margin: 10.
+    },
+    contentButtons:{
+        flex:1,
+        flexDirection: "row",
+        gap:10,
+        justifyContent: "space-around",
+    }
+});
